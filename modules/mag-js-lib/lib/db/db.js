8fd19eca64ba706b59d7341e89690488325232c5
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2020-01-12 23:47:49 +0100

    .

--- modules/mag-js-lib/lib/db/db.js
+++ modules/mag-js-lib/lib/db/db.js
@@ -5,19 +5,22 @@ var debug = process.env.DEBUG || false;
 
 var variables = {
     dbms : '',
+    driver : '',
     server : '',
     username : '',
     password : ''
 }
 
-function init(dbms, server, username, password){
+function init(dbms, driver, server, username, password){
     variables.dbms = dbms;
+    variables.driver = driver;
     variables.server = server;
     variables.username = username;
     variables.password = password;
     //console.log(debug);
     if (debug){
         console.log(`dbms = ${dbms}`);
+        console.log(`driver = ${driver}`);
         console.log(`server = ${server}`);
         console.log(`username = ${username}`);
         console.log(`password = ${password}`);
