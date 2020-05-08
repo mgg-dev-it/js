f25ec5f7c2e5d877509c11160aa2f7ea7f444099
mgg-dev-it <44374657+mgg-dev-it@users.noreply.github.com>
2020-05-09 00:33:56 +0200

    .

--- modules/mag-js-lib/lib/game/letlab.js
+++ modules/mag-js-lib/lib/game/letlab.js
@@ -4,6 +4,7 @@ var matrix_width, matrix_height;
 var characters;
 var start_end_distance;
 var matrix;
+var startCell, endCell;
 
 function init() {
     matrix_width = 7;
@@ -16,13 +17,14 @@ function init() {
 function initMatrix() {
     let rowString = ' '.repeat(matrix_width);
     //console.log(rowString);
-    var rowArray = rowString.split('');
+    //    let rowArray = rowString.split('');
     //console.log(rowArray);
     //console.log(rowArray.toString());
     //console.log('[' + rowArray.join('][') + ']');
     matrix = [];
     for (let i = 0; i < matrix_height; i++) {
         //console.log(i);
+        let rowArray = rowString.split('');
         matrix.push(rowArray);
     }
     //console.log(matrix);
@@ -37,9 +39,82 @@ function displayMatrix() {
     console.log(s);
 }
 
+function generateStartAndEnd() {
+    let sides = ['left', 'top', 'right', "bottom"];
+    let startSide = sides.splice(Math.floor(Math.random() * 4), 1)[0];
+    //console.log(startSide);
+    let endSide = sides.splice(Math.floor(Math.random() * 3), 1)[0];
+    //console.log(endSide);
+    switch (startSide) {
+        case 'left':
+            startCell = new Cell(Math.floor(Math.random() * matrix_height), 0);
+            break;
+        case 'top':
+            startCell = new Cell(0, Math.floor(Math.random() * matrix_width));
+            break;
+        case 'right':
+            startCell = new Cell(Math.floor(Math.random() * matrix_height), matrix_width - 1);
+            break;
+        case 'bottom':
+            startCell = new Cell(matrix_height - 1, Math.floor(Math.random() * matrix_width));
+            break;
+        default:
+    }
+    //console.log(`startCell at ${startSide} = ${startCell.toString()}`);
+    switch (endSide) {
+        case 'left':
+            endCell = new Cell(Math.floor(Math.random() * matrix_height), 0);
+            break;
+        case 'top':
+            endCell = new Cell(0, Math.floor(Math.random() * matrix_width));
+            break;
+        case 'right':
+            endCell = new Cell(Math.floor(Math.random() * matrix_height), matrix_width - 1);
+            break;
+        case 'bottom':
+            endCell = new Cell(matrix_height - 1, Math.floor(Math.random() * matrix_width));
+            break;
+        default:
+    }
+    //console.log(`endCell at ${endSide} = ${endCell.toString()}`);
+}
+
+function isStartAndEndOK() {
+    if (startCell.row == endCell.row && startCell.col == endCell.col) return (false);
+    if (startCell.row == 0 && endCell.row == matrix_height - 1) return (true);
+    if (startCell.row == matrix_height - 1 && endCell.row == 0) return (true);
+    if (startCell.col == 0 && endCell.col == matrix_width - 1) return (true);
+    if (startCell.col == matrix_width - 1 && endCell.col == 0) return (true);
+    let distance = Math.abs(startCell.row - endCell.row) + Math.abs(startCell.col - endCell.col);
+    //console.log(`distance = ${distance}`);
+    if (distance <= Math.min(matrix_width, matrix_height) - 2) return (false);
+    return (true);
+}
+
+class Cell {
+    constructor(row, col) {
+        this.row = row;
+        this.col = col;
+    }
+    toString() {
+        return (`Cell(row=${this.row} col=${this.col})`);
+    }
+}
+
 function letterLabyrinth() {
     console.log('letterLabyrinth');
     init();
+    let i = 0;
+    do {
+        generateStartAndEnd();
+        i++;
+    } while (i < 10 && !isStartAndEndOK());
+    if (!isStartAndEndOK()) {
+        console.log('Sorry, something went wrong during the creation - please try again!');
+        return;
+    }
+    matrix[startCell.row][startCell.col] = 'S';
+    matrix[endCell.row][endCell.col] = 'E';
     displayMatrix();
 }
 
